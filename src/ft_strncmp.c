91a99da6cdba45a3d1175704b784329ec2c6ba8b
Jose Boon <jboon@student.codam.nl>
2025-05-09 17:55:43 +0200

    Updated libft with strcmp, stratoll, strstr, strrstr, strcpy, and fixed strtrim

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jboon <jboon@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 14:01:42 by jboon             #+#    #+#             */
-/*   Updated: 2024/10/14 11:03:31 by jboon            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_strncmp.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jboon <jboon@student.42.fr>                  +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/10/07 14:01:42 by jboon         #+#    #+#                 */
+/*   Updated: 2025/05/09 17:45:49 by jboon         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] == s2[i] && i < n && s1[i] && s2[i])
 		++i;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 == *s2 && *s1)
+	{
+		++s1;
+		++s2;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
